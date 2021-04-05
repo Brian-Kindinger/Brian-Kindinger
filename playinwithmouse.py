1747eff9b1dd2ee770b1ff7544c6c8c343a8beae
Brian Kindinger <brian.kindinger@opal-rt.com>
2021-04-05 13:10:36 -0400

    fun stuff

--- playinwithmouse.py
+++ playinwithmouse.py
@@ -1,3 +1,34 @@
-import pyautogui
+from pynput.mouse import Listener
+from pynput.mouse import Button, Controller
+import time 
 
-print("Hello world")
+mouse = Controller()
+gTime = 0.0
+gPressed = False
+
+def on_click(x, y, button, pressed):
+    global gTime
+    global gPressed
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    if pressed:
+        gTime=time.time()*1000.0
+        gPressed = True
+    if not pressed:
+        gPressed = False
+
+
+with Listener(on_click=on_click) as listener:
+    listener.join()
+
+def main():
+    print("Hello World!")
+    if gPressed == True:
+        if ((gTime - time.time()*1000) > 1000):
+            print(gTime)
+            mouse.click(Button.left, 2)
+    time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
